318bd412745d82607b98d4e6518a49bd807c8247
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2023-05-29 23:16:02 +0400

    Add files via upload

--- src/main/java/furnygo/sptp/SpTpConfigModel.java
+++ src/main/java/furnygo/sptp/SpTpConfigModel.java
@@ -6,10 +6,10 @@ import io.wispforest.owo.config.annotation.Modmenu;
 @Modmenu(modId = "sptp")
 @Config(name = "sptp-config", wrapperName = "SpTpConfig")
 public class SpTpConfigModel {
-    public boolean useTeleportToRegion = true;
+    public boolean useTeleportToRegion = false;
     public String teleportToRegionCommand = "warp free_spectator";
     public int teleportToRegionCooldown = 100;
     public String creativeCommand = "gamemode creative";
     public String survivalCommand = "gamemode survival";
-    public String spectatorCommand = "gamemode survival";
+    public String spectatorCommand = "gamemode spectator";
 }
